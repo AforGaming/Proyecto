eae0974aa8a2bfd97ec6a7ba04eb572f991d8fc9
Aaron Noguera <aaronnoguera07@gmail.com>
2020-12-20 04:49:16 -0300

    Un detalle

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -6,5 +6,6 @@
 <b> Debe crear un Empleado, un Proveedor, un Cliente, una Categoria y un Producto </b> </b> <br />
 <b> Luego se podran hacer compras y automaticamente el stock se actualizara </b> </b> <br />
 <b> Hay reportes de compras, ventas y ganancias filtras por dia, mes y anio </b> </b> <br />
+<b> Al hacer una compra o venta, automaticamente se generara el importe segun la cantidad del producto seleccionado </b> </b> <br />
 
 @include('templates/footer')
